63f94553acedce47972d668f5c79964c2cf6f93a
= <=>
2023-05-06 22:58:25 +0800

    Result

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,5 +1,7 @@
 # 最终在main函数中传入一个维度为6的numpy数组，输出预测值
 
+# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
+
 import os
 
 try:
@@ -8,64 +10,73 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-class Ridge_:
-    def __init__(self, alpha=1, copy_X=True, normalize=True,
-                 solver='gd', solve_dJ='debug', n_iters=1000, learning_rate=0.1, tol=1e-3):
-        # n_iters、 learning_rate、 tol在选用梯度下降和随机梯度下降法时有效， solve_dJ仅在选择梯度下降法时有效
-        assert solver in ['gd', 'sgd'], \
-            'solver must be one of "gd", "sgd", and "{}" got!'.format(solver)
-        assert solve_dJ in ['debug'], \
-            'solve_dJ must be one of "debug", and "{}" got!'.format(solve_dJ)
-        self.alpha = alpha
-        self.copy_X = copy_X
-        self.normalize = normalize
-        self.solver = solver
-        self.solve_dJ = solve_dJ
-        self.n_iters = n_iters
-        self.learning_rate = learning_rate
-        self.tol = tol
-        
-    def _fit_lsq(self, X_b, y):  # 最小二乘法训练模型
-        XT_X = X_b.T.dot(X_b) + self.alpha * np.eye(X_b.shape[1])
-        self._theta = np.linalg.inv(XT_X).dot(X_b.T.dot(y))  # (X^T * X + alpha*I)^-1 * (X^T * y)
-        self.coef_ = self._theta[1:]
-        self.intercept_ = self._theta[0]
+def ridge_train(x, y):
+    lbda = 0.03
+    m = x.shape[0]
+    x = np.concatenate((np.ones((m,1)),x),axis=1)
+    I = np.eye(x.shape[1])
+    res = np.dot(x.T,x) + lbda * I
+    res_inv = np.linalg.inv(res)
+    w = np.dot(res_inv, np.dot(x.T, y))
+    return w
 
-class Lasso_:
-    def __init__(self, alpha=1, copy_X=True, normalize=True,
-                 solver='gd', solve_dJ='debug', n_iters=1000, learning_rate=0.1, tol=1e-3):
-        # n_iters、 learning_rate、 tol在选用梯度下降和随机梯度下降法时有效， solve_dJ仅在选择梯度下降法时有效
-        assert solver in ['gd', 'sgd'], \
-            'solver must be one of "gd", "sgd", and "{}" got!'.format(solver)
-        assert solve_dJ in ['debug'], \
-            'solve_dJ must be one of "debug", and "{}" got!'.format(solve_dJ)
-        self.alpha = alpha
-        self.copy_X = copy_X
-        self.normalize = normalize
-        self.solver = solver
-        self.solve_dJ = solve_dJ
-        self.n_iters = n_iters
-        self.learning_rate = learning_rate
-        self.tol = tol
+def ridge(data):
+    x, y = read_data()
+    weight = ridge_train(x, y)
+    data = np.insert(data, 0, 1)
+    return data @ weight + 0.5
 
-    def _fit_gd(self, X_b, y):  # 梯度下降法训练模型
-        theta = np.ones(X_b.shape[1])
-        for _ in range(self.n_iters):
-            dJ = _dJ_gd_debug(theta, X_b, y, J='Lasso', alpha=self.alpha)
-            theta -= self.learning_rate * dJ
-            if np.linalg.norm(dJ) <= self.tol:  # 提前终止条件
-                break
-        self._theta = theta
-        self.coef_ = self._theta[1:]
-        self.intercept_ = self._theta[0]
 
-def main(data):
-    x,y = read_data()
-    weight1 = Ridge_(x,y)
-    weight2 = Lasso_(x,y)
-    return weight1,weight2
+def lasso_train(x, y):
+    m = x.shape[0]
+    x = np.concatenate((np.ones((m,1)),x),axis=1)
+    tmpn = x.shape[1]
+    for i in range(1, tmpn):
+        for j in range(i + 1, tmpn):
+            if(i == j - 1):
+                tmp = x[:, i] * x[:, j]
+                x = np.column_stack((x, tmp))
+            else:  break
+    n = x.shape[1]
+    w = np.zeros(n)
+    w = w.reshape(-1, 1)
+    #print(np.sign(w))
+    max_iterator = 1000000
+    alpha = 8e-10
+    lbda = 0.03
+    y = y.reshape(y.shape[0], 1)
+    
+    #print(y)
+    #print(m)
+    for i in range(max_iterator):
+        gradient = np.dot(x.T,(np.dot(x, w)-y)) / m + lbda * np.sign(w)
+        #print(gradient)
+        w = w - alpha * gradient 
+        #print(w)   
+    return w
 
-def read_data(path='C:/Users/EthanCai/linear-regression-ccyyqq981217/data/exp02'):
+def lasso(data):
+    x, y = read_data()
+    weight = lasso_train(x, y)
+    data = np.insert(data, 0, 1)
+    data = data.reshape(data.shape[0], 1)
+    tmpn = data.shape[0]
+    for i in range(1, tmpn):
+        for j in range(i + 1, tmpn):
+            if(i == j - 1):
+                tmp = data[i] * data[j]
+                data = np.append(data, tmp)
+                #print(data.shape)
+            else: break 
+    #a = np.append(data, [data[1] * data[2], data[3] *data[4], data[5] * data[6]])
+    data = data.reshape(1, data.shape[0])
+    #print(weight.shape)
+    #print(data.shape)
+    ans = np.dot(data, weight)
+    #print(ans)
+    return ans[0][0]
+
+def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
